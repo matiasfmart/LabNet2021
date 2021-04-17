1ce6371dd28d9ee73e27975478fa6839f4314fde
Matias Federico Martinez <martinez_matias@live.com.ar>
2021-04-17 16:45:23 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,28 +11,31 @@ namespace ejercicio1_poo
     {
         static void Main(string[] args)
         {
-            Random aleatorio = new Random();
-            int pasajerosAuto;
+            Random aleatorio = new Random(); //instancio clase random para enviar a los objetos transportes
+            int pasajerosAuto; 
             int pasajerosAvion;
 
-            List<Transporte> transportes = new List<Transporte>();
+            List<Transporte> transportes = new List<Transporte>();  //declaro lista de objetos del tipo transporte
 
+            /* Mediante un for instancio los objetos avion y automoviles y asigno a las variables de los pasajeros un num aleatorio */
             for (int i = 0; i < 5; i++)
             {
-                pasajerosAuto = aleatorio.Next(1, 6);
-                pasajerosAvion = aleatorio.Next(1, 200);
+                pasajerosAuto = aleatorio.Next(1, 6);   //asigno un numero aleatorio entre 1-6 ya que es la cantidad de pasajeros que entran en un auto estandar
+                pasajerosAvion = aleatorio.Next(1, 200);    //asigno entre 1-200 ya que es de manera estandar los pasajeros en un avion
 
                 Avion avion = new Avion();
                 Automovil auto = new Automovil();
 
                 avion.Pasajeros = pasajerosAvion;
                 auto.Pasajeros = pasajerosAuto;
-
+                /* agrego los objetos a la lista */
                 transportes.Add(auto);
                 transportes.Add(avion);
             }
 
-            int nro = 1;
+            int nro = 1; //variable para mostrar el nro del transporte a mostrar
+            
+            /* mediante for muestro los datos primero de los aviones y luego de los autos como esta especificado en la consigna */
             for (int i = 0; i < transportes.Count; i++)
             {
                 if (transportes[i] is Avion)
