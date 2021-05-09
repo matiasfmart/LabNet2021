3400404f5714f7b1a9fc950639a51d867036aba1
matiasfmart <matiasfmart@MacBook-Pro-de-Matias.local>
2021-05-09 20:28:30 -0300

    commit 6th exercise

---
+++ Ejercicio6.MVC.Logic/EmployeesLogic.cs
@@ -0,0 +1,40 @@
+﻿using Ejercicio6.MVC.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6.MVC.Logic
+{
+    public class EmployeesLogic : BaseLogic, ABMInterface<Employees>
+    {
+        public void Add(Employees toAdd) //Se agrega un nuevo elemento
+        {
+            context.Employees.Add(toAdd);
+            context.SaveChanges();
+        }
+
+        public void Delete(int toDelete) //Mediante ID se elimina el elemento seleccionado
+        {
+            var empToDelete = context.Employees.Find(toDelete);
+            context.Employees.Remove(empToDelete);
+            context.SaveChanges();
+        }
+
+        public List<Employees> GetAll() //Se traen todos los datos
+        {
+            return context.Employees.ToList();
+        }
+
+        public void Update(Employees toUpDate) //Se modifican los datos
+        {
+            var employeeUpDate = context.Employees.Find(toUpDate.EmployeeID);
+
+            employeeUpDate.FirstName = toUpDate.FirstName;
+            employeeUpDate.LastName = toUpDate.LastName;
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Ejercicio6.MVC.Project/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejercicio6.MVC.Project.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
